740a2fd48727cd6795cf954687b25080573399de
Anthony Birkett <anthony@a-birkett.com>
2014-06-18 14:35:04 +0100

    Handler now redirects to home if called without parameters Made the link check movable across domains

--- config.php
+++ config.php
@@ -10,8 +10,10 @@ ini_set("display_errors", 1);
 //Timezone for converting timestamps
 date_default_timezone_set("Europe/London");
 
-//This is where the site is hosted. Allows the service to move
+//This is where the site is hosted. Allows the service to move, should include protocol
 define('DOMAIN_NAME', 'http://birk.it/');
+//This is used in regex, should be the bare domain, no protocol, subdomain or slashes
+define('BASIC_DOMAIN_NAME', 'birk.it');
 
 //Database connection 
 define('DATABASE_HOSTNAME', 'host');
