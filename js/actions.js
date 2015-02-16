5617cc427ad92e6b7498fd3365781f83ed5dbe82
Anthony Birkett <anthony@a-birkett.com>
2015-02-16 15:22:52 +0000

    Cleanup. Aiming for PSR2 compliance.

--- js/actions.js
+++ js/actions.js
@@ -1,19 +1,26 @@
-﻿function addslashes(str) { return (str + '').replace(/[\\"']/g, '\\$&').replace(/\u0000/g, '\\0'); }
+﻿function addslashes(str)
+{
+    return (str + '').replace(/[\\"']/g, '\\$&').replace(/\u0000/g, '\\0');
+}
+
 function doaction()
 {
     $(".box").hide("slow");
     $(".box").promise().done(function () {
-		var box = document.getElementById("box");
-		box.className = "box";
-        if (window.XMLHttpRequest) { xmlhttp = new XMLHttpRequest(); }
-        else { xmlhttp = new ActiveXObject("Microsoft.XMLHTTP"); }
+        var box = document.getElementById("box");
+        box.className = "box";
+        if (window.XMLHttpRequest) {
+            xmlhttp = new XMLHttpRequest();
+        } else {
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
         xmlhttp.onreadystatechange = function () {
             if (xmlhttp.readyState == 4) {
                 switch (xmlhttp.status) {
                     case 200: box.className += " success"; break;
                     case 400: box.className += " failed"; break;
                 }
-				box.value = xmlhttp.response;
+                box.value = xmlhttp.response;
             }
         }
         xmlhttp.open("POST", "handler.php", true);
