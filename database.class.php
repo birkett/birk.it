7e3d69d045f0add50e3993022cdac6e3b73b2f6e
Anthony Birkett <anthony@a-birkett.com>
2015-02-17 12:38:23 +0000

    Many cleanups based on Squiz recommendations.

--- database.class.php
+++ database.class.php
@@ -1,9 +1,16 @@
 <?php
-//-----------------------------------------------------------------------------
-// Database class
-//
-//  Basic class to interface with a MySQLi database
-//-----------------------------------------------------------------------------
+/**
+ * Basic class to interface with a SQLite database
+ *
+ * PHP Version 5.4
+ *
+ * @category  Database
+ * @package   Birk.it
+ * @author    Anthony Birkett <anthony@a-birkett.co.uk>
+ * @copyright 2015 Anthony Birkett
+ * @license   http://opensource.org/licenses/MIT MIT
+ * @link      http://birk.it
+ */
 
 namespace ABirkett;
 
@@ -11,79 +18,94 @@ use PDO;
 
 class Database
 {
-    private $mLink; //Store the connection link
+    /**
+     * Store the current link to avoid reconnections
+     * @var object $mLink
+     */
+    private $mLink;
 
-    //-----------------------------------------------------------------------------
-    // Constructor
-    //        In: none
-    //        Out: none
-    //-----------------------------------------------------------------------------
+    /**
+     * Constructor
+     * @return none
+     */
     public function __construct()
     {
         try {
-            $this->mLink = new PDO("sqlite:" . DATABASE_FILENAME);
+            $this->mLink = new PDO('sqlite:'.DATABASE_FILENAME);
             $this->mLink->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (\PDOException $e) {
-            echo "Unable to connect to database";
+            echo 'Unable to connect to database';
         }
-    }
 
-    //-----------------------------------------------------------------------------
-    // Destructor
-    //        In: none
-    //        Out: none
-    //-----------------------------------------------------------------------------
+    }//end __construct()
+
+
+    /**
+     * Destructor
+     * @return none
+     */
     public function __destruct()
     {
         $this->mLink = null;
-    }
 
-    //-----------------------------------------------------------------------------
-    // getInstance
-    //        In: none
-    //        Out: none
-    //-----------------------------------------------------------------------------
+    }//end __destruct()
+
+
+    /**
+     * Open a database handle
+     * @return object Database handle
+     */
     public static function getInstance()
     {
         static $database = null;
-        if (!isset($database)) {
+        if (isset($database) === false) {
             $database = new Database();
         }
-        return $database;
-    }
 
-    //-----------------------------------------------------------------------------
-    // Run a query
-    //        In: Query string, Parameters array
-    //        Out: Result
-    //-----------------------------------------------------------------------------
+        return $database;
+
+    }//end getInstance()
+
+
+    /**
+     * Run a query
+     * @param string  $query  Query string to run.
+     * @param mixed[] $params Array of parameters to bind.
+     * @return mixed[] Array of results
+     */
     public function runQuery($query, $params = array())
     {
         if (!$this->mLink) {
             return;
         }
+
         $statement = $this->mLink->prepare($query);
         $statement->execute($params);
         if ($statement->columnCount() != 0) {
             return $statement->fetchAll();
         }
-    }
 
-    //-----------------------------------------------------------------------------
-    // Get single row from a result
-    //        In: MySQLi result
-    //        Out: Single row
-    //   Returns next row on each call until end, then NULL
-    //-----------------------------------------------------------------------------
+    }//end runQuery()
+
+
+    /**
+     * Get single row from a result, until no results left
+     * @param mixed[] $result Array of rows
+     * @return mixed[] One row array or null when none left
+     */
     public function getRow(&$result)
     {
-        if (!$result) {
+        if ($result === false) {
             return;
         }
+
         if (count($result) != 0) {
             return array_shift($result);
         } else {
             return null;
         }
-    }
-}
+
+    }//end getRow()
+
+
+}//end class
--- js/index.php
+++ js/index.php
@@ -1,2 +1,15 @@
 <?php
+/**
+ * Small index page to prevent browser navigation to this folder
+ *
+ * PHP Version 5.4
+ *
+ * @category  Index
+ * @package   Birk.it
+ * @author    Anthony Birkett <anthony@a-birkett.co.uk>
+ * @copyright 2015 Anthony Birkett
+ * @license   http://opensource.org/licenses/MIT MIT
+ * @link      http://birk.it
+ */
+
 header('Location: /');
