1509cc7dec043abb6b4afd67cb868c59c9c8a3b5
Anthony Birkett <anthony@a-birkett.com>
2015-02-17 11:10:47 +0000

    Restructuring.

--- functions.php
+++ functions.php
@@ -3,10 +3,18 @@
 // Handler
 //  Handles both AJAX and GET requests.
 //-----------------------------------------------------------------------------
-require_once("config.php");
 require_once("database.class.php");
 
 use ABirkett\Database as Database;
+
+function PHPDefaults()
+{
+    //Show PHP errors and warnings
+    error_reporting(E_ALL);
+    ini_set("display_errors", 1);
+    //Timezone for converting timestamps
+    date_default_timezone_set("Europe/London");
+}
 
 //-----------------------------------------------------------------------------
 // Generate()
@@ -83,46 +91,14 @@ function Finish($msg, $code)
     exit($msg);
 }
 
-//-----------------------------------------------------------------------------
-// Main logic
-//-----------------------------------------------------------------------------
-PHPDefaults();
-
-if (isset($_POST['input'])) {
-    //We are in generate mode
-    if ($_POST['input'] == "") {
-        Finish("URL cannot be blank", 400);
-    }
-    if (strlen($_POST['input']) > 2048) {
-        Finish("Input URL too long!", 400);
-    }
-
-    //Remove http:// or https://
-    $inputurl = preg_replace('#^https?://#', '', strtolower($_POST['input']));
-    //Remove a trailing backslash
-    $inputurl = rtrim($inputurl, "/");
-
-    //Check if link is already shortened, and return the full URL instead
-    if (preg_match("/".BASIC_DOMAIN_NAME."/i", $inputurl)) {
-        $original = SwapURL($inputurl);
-        if ($original) {
-            Finish("http://".$original, 200); //Add the http back in
-        }
-        Finish("Not found!", 400);
-    }
-
-    if (!SwapURL($inputurl)) {
-        AddNewUrl($inputurl);
-    }
-    Finish(DOMAIN_NAME . SwapURL($inputurl), 200);
-} elseif (isset($_GET['url'])) {
-    //We are in fetch mode
-    $originalurl = SwapURL(DOMAIN_NAME . "/" . $_GET['url']);
-
-    if ($originalurl) {
-        Redirect("http://".$originalurl); //If found, redicrect
-    } else {
-        Redirect(DOMAIN_NAME); //Not found, go home
-    }
+/**
+* Replace a tag with a string (for inserting sub templates into the output)
+* @param string $tag    Tag to replace
+* @param string $string String that will replace Tag
+* @param string $output Unparsed template passed by reference
+* @return none
+*/
+function replaceTag($tag, $string, &$output)
+{
+    $output = str_replace($tag, $string, $output);
 }
-Redirect(DOMAIN_NAME); //Do nothing when not called correctly
