9d33b921a33faa11b34f61d5822b035fdbe54ba7
Anthony Birkett <anthony@a-birkett.com>
2015-02-19 17:27:24 +0000

    No longer directly accessing GET and POST data, use filter_input instead. Minor cleanup.

--- js/actions.js
+++ js/actions.js
@@ -6,31 +6,33 @@
 function doaction()
 {
     $(".box").hide("slow");
-    $(".box").promise().done(function () {
-        var box = document.getElementById("box");
-        box.className = "box";
-        if (window.XMLHttpRequest) {
-            xmlhttp = new XMLHttpRequest();
-        } else {
-            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-        }
-
-        xmlhttp.onreadystatechange = function () {
-            if (xmlhttp.readyState === 4) {
-                switch (xmlhttp.status) {
-                    case 200: box.className += " success"; break;
-                    case 400: box.className += " failed"; break;
-                }
-
-                box.value = xmlhttp.response;
+    $(".box").promise().done(
+        function () {
+            var box = document.getElementById("box");
+            box.className = "box";
+            if (window.XMLHttpRequest) {
+                xmlhttp = new XMLHttpRequest();
+            } else {
+                xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
             }
 
+            xmlhttp.onreadystatechange = function () {
+                if (xmlhttp.readyState === 4) {
+                    switch (xmlhttp.status) {
+                        case 200: box.className += " success"; break;
+                        case 400: box.className += " failed"; break;
+                    }
+
+                    box.value = xmlhttp.response;
+                }
+
+            }
+            xmlhttp.open("POST", "index.php", true);
+            var inputurl = encodeURIComponent(addslashes(box.value));
+            xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+            xmlhttp.send("&input=" + inputurl);
         }
-        xmlhttp.open("POST", "index.php", true);
-        var inputurl = encodeURIComponent(addslashes(box.value));
-        xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-        xmlhttp.send("&input=" + inputurl);
-    });
+    );
     $(".box").show("slow");
 
 }
